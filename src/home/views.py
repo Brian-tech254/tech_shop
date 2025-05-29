7c65500887975ec117cfeb5278dfb7eadad604d7
Brian-tech254git config --global user.email kiokog520@gmail.comgit config --global user.name Brian-tech254 <kiokog520@gmail.com>
2025-05-29 12:12:48 +0300

    commit

--- src/home/views.py
+++ src/home/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    return render(request, 'base.html')
+
+def products(request):
+    return render(request, 'home/products.html')
+
+def products(request):
+    context ={
+        'products' : [
+            {'name': 'apple watch',
+             'price': 599.00,
+             'description': 'aluminium case,starlight sport'},
+
+             {'name': 'iphone 14',
+              'price': 799.00,
+              'description': '128gb storage 12gb ram'},
+
+              {'name': 'airpods pro',
+               'price': 78.00,
+               'description':'active noise collection'}
+        ]
+    }
+    return render(request, 'home/products.html', context)
